7d2c8351cce9baf03415bfb8ac633c58ab2cbbf4
Landon Rehn <lando.rehn@gmail.com>
2016-04-03 12:08:10 -0500

    Comments
    Added comments

--- Personal-Recipe-Scanner/DbManager.cpp
+++ Personal-Recipe-Scanner/DbManager.cpp
@@ -7,9 +7,11 @@ SqlLite::SqlLite(QWidget *parent) :
 {
     ui->setupUi(this);
 
+
+    //needed to load the database file. Needs to be //whatever location you have the file in
     QSqlDatabase mydb=QSqlDatabase::addDatabase("QSQLITE");
     mydb.setDatabaseName("C:/Sqlite3/scanner.db");
-
+//test to see if it's connected or not
     if(!mydb.open())
     {
         ui->label->setText("Failed to open database");
