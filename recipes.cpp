87e3f19003884337a240ef13976f3a6f91eaa28b
Chelsea Patek <pateca26@uwgb.edu>
2016-05-13 15:33:13 -0500

    Update recipes.cpp

--- recipes.cpp
+++ recipes.cpp
@@ -1,6 +1,5 @@
 #include "recipes.h"
 #include "ui_recipes.h"
-#include<QMessageBox>
 
 Recipes::Recipes(QWidget *parent) :
     QDialog(parent),
@@ -8,117 +7,183 @@ Recipes::Recipes(QWidget *parent) :
 
 {
     ui->setupUi(this);
-
-      Login conn;
-            if(!conn.connOpen())
-            {
-                ui->label_sec_status->setText("Failed to connect to DB");
-            }
-            else
-            {
-                ui->label_sec_status->setText("Connected to DB");
-            }
-
 }
 
 Recipes::~Recipes()
 {
     delete ui;
 }
-QString filename;
-void Recipes::on_pushButton_clicked()
+
+void Recipes::setUserId(QString id)
 {
+    userID = id;
+}
 
-    Login conn;
-    QString rid,recipe,ingredients,instructions;
-    rid = ui->rid->text();
-    recipe = ui->txt_recipe->text();
-    ingredients = ui->lineEdit_Ingredients->text();
-    instructions = ui->lineEdit_Instructions->text();
-
-    if(!conn.connOpen())
+void Recipes::on_pushButton_Save_clicked()
+{
+    // check if updating a recipe
+    if(!ui->lineEdit_RecipeID->text().isEmpty())
     {
-        qDebug()<<"Failed to open DB";
+        updateRecipe();
         return;
     }
 
-    //open db
-    conn.connOpen();
-
-    //sql query
-    QSqlQuery qry;
-    qry.prepare("INSERT INTO recipe (rid,recipe,ingredients,instructions) values ('"+rid+"','"+recipe+"','"+ingredients+"','"+instructions+"')");
-
-    if(qry.exec())
+    // add new recipe
+    Login conn;
+    bool dbOK = conn.connOpen();
+    if(dbOK)
     {
-        QMessageBox::critical(this,tr("Save"),tr("Saved"));
+        QString recipe,ingredients,instructions;
+        recipe= ui->lineEdit_Name->text();
+        ingredients = ui->lineEdit_Ingredients->text();
+        instructions = ui->lineEdit_Instructions->text();
+
+        //sql query
+        QSqlQuery qry;
+        qry.prepare("INSERT INTO RecipeInfo(userID,recipe,ingredients,instructions) values ('"+userID+"','"+recipe+"','"+ingredients+"','"+instructions+"')");
+
+        if(qry.exec())
+        {
+            QMessageBox::
+            QMessageBox::information(this,tr("Save"),tr("Saved"));
+        }
+        else
+        {
+            QMessageBox::critical(this,tr("Error"),qry.lastError().text());
+        }
+
+        //reset variables
+        on_pushButton_Clear_clicked();
+        on_pushButton_Search_clicked();
+
+        //close connection
         conn.connClose();
     }
     else
-    {
-        QMessageBox::critical(this,tr("Error"),qry.lastError().text());
-    }
+        qDebug() << "Unable to connect to database";
 }
 
 
 //Update Recipe
-void Recipes::on_pushButton_Edit_clicked()
+void Recipes::updateRecipe()
 {
     Login conn;
-    QString rid,recipe,ingredients,instructions;
-    rid = ui->rid->text();
-    recipe = ui->txt_recipe->text();
-    ingredients = ui->lineEdit_Ingredients->text();
-    instructions = ui->lineEdit_Instructions->text();
-
-    if(!conn.connOpen())
+    bool dbOK = conn.connOpen();
+    if(dbOK)
     {
-        qDebug()<<"Failed to open DB";
-        return;
-    }
+        QString recipeID, recipe,ingredients,instructions;
+        recipeID = ui->lineEdit_RecipeID->text();
+        recipe = ui->lineEdit_Name->text();
+        ingredients = ui->lineEdit_Ingredients->text();
+        instructions = ui->lineEdit_Instructions->text();
 
-    //open db
-    conn.connOpen();
+        //sql query
+        QSqlQuery qry;
 
-    //sql query
-    QSqlQuery qry;
-    qry.prepare("UPDATE recipe SET rid = '"+rid+"',recipe = '"+recipe+"' ,ingredients = '"+ingredients+"' ,instructions = '"+instructions+"' where rid = '"+rid+"'");
+        qry.prepare("UPDATE RecipeInfo SET recipe = '"+recipe+"' ,ingredients = '"+ingredients+"' ,instructions = '"+instructions+"' where recipeID = '"+recipeID+"'");
 
-    if(qry.exec())
-    {
-        QMessageBox::critical(this,tr("Update"),tr("Updated"));
+        if(qry.exec())
+        {
+            QMessageBox::information(this,tr("Update"),tr("Updated"));
+            conn.connClose();
+        }
+        else
+        {
+            QMessageBox::critical(this,tr("Error:"),qry.lastError().text());
+        }
+
+        //clear line edits
+        on_pushButton_Clear_clicked();
+        on_pushButton_Search_clicked();
+
+        //close connection
         conn.connClose();
     }
     else
-    {
-        QMessageBox::critical(this,tr("Error:"),qry.lastError().text());
-    }
+        qDebug() << "Unable to connect to database";
 }
 
-//load recipe
-void Recipes::on_pushButton_load_clicked()
+void Recipes::on_tableView_Recipes_clicked(const QModelIndex &index)
+{
+    ui->lineEdit_RecipeID->setText(index.sibling(index.row(), 0).data().toString());
+    ui->lineEdit_Name->setText(index.sibling(index.row(), 1).data().toString());
+    ui->lineEdit_Ingredients->setText(index.sibling(index.row(), 2).data().toString());
+    ui->lineEdit_Instructions->setText(index.sibling(index.row(), 3).data().toString());
+}
+
+void Recipes::on_pushButton_Clear_clicked()
+{
+    ui->lineEdit_RecipeID->clear();
+    ui->lineEdit_Name->clear();
+    ui->lineEdit_Ingredients->clear();
+    ui->lineEdit_Instructions->clear();
+}
+
+void Recipes::on_pushButton_Delete_clicked()
 {
     Login conn;
-    QString rid;
-    rid = ui->lineEdit_Load->text();
-    QSqlQueryModel * model = new QSqlQueryModel();
+    bool dbOK = conn.connOpen();
+    if(dbOK)
+    {
+        /*deleting a recipe from the list*/
+        if (!ui->lineEdit_RecipeID->text().isEmpty())
+        {
+            QMessageBox::StandardButton reply;
 
-    //open db
-    conn.connOpen();
+            reply=QMessageBox::question(this,"Delete","Are you sure you want to delete this recipe?",QMessageBox::Yes|QMessageBox::No);
 
-    //SQL query
-    QSqlQuery qry;
+            if(reply==QMessageBox::Yes)
+            {
+                QString recipeID = ui->lineEdit_RecipeID->text();
 
-    qry.prepare("SELECT recipe,ingredients,instructions FROM recipe WHERE rid = '"+rid+"'");
+                //SQL query
+                QSqlQuery qry;
 
-    qry.exec();
-    model->setQuery(qry);
-    ui->tableView->setModel(model);
+                qry.exec("DELETE FROM RecipeInfo WHERE recipeID = '" + recipeID + "'");
 
-    //Close DB
-    conn.connClose();
-    qDebug() << (model->rowCount());
+                //Close DB
+                conn.connClose();
 
-
+                on_pushButton_Clear_clicked();
+                on_pushButton_Search_clicked();
+            }
+        }
+    }
+    else
+        qDebug() << "Unable to connect to database";
 }
 
+void Recipes::on_pushButton_Search_clicked()
+{
+    Login conn;
+    bool dbOK = conn.connOpen();
+    if(dbOK)
+    {
+        QString search = ui->lineEdit_Search->text();
+        QSqlQueryModel * model = new QSqlQueryModel();
+
+        //SQL query
+        QSqlQuery qry;
+        QString qryString;
+
+        if(!search.isEmpty())
+        {
+            qryString = "SELECT recipeID,recipe,ingredients,instructions FROM RecipeInfo WHERE userID = '" + userID +
+                    "' AND (recipe LIKE '%"+search+"%' OR ingredients LIKE  '%"+search+"%')";
+        }
+        else
+        {
+            qryString = "SELECT recipeID,recipe,ingredients,instructions FROM RecipeInfo WHERE userID = '" + userID +"'";
+        }
+
+        qry.exec(qryString);
+        model->setQuery(qry);
+
+        ui->tableView_Recipes->setModel(model);
+
+        //Close DB
+        conn.connClose();
+    }
+    else
+        qDebug() << "Unable to connect to database";
+}
