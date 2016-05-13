5b480197de9421d30e92ebf23ad9122a41a96ed5
Chelsea Patek <pateca26@uwgb.edu>
2016-05-13 15:28:09 -0500

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -6,16 +6,6 @@ Login::Login(QWidget *parent) :
     ui(new Ui::Login)
 {
     ui->setupUi(this);
-
-
-    if(!connOpen())
-    {
-        ui->label->setText("Failed to connect to DB");
-    }
-    else
-    {
-        ui->label->setText("Connected to DB");
-    }
 }
 
 Login::~Login()
@@ -23,46 +13,120 @@ Login::~Login()
     delete ui;
 }
 
-void Login::on_pushButton_clicked()
+void Login::connClose()
 {
-    QString Username,Password;
-    Username = ui->lineEdit_Username->text();
-    Password = ui->lineEdit_Password->text();
+    RDB.close();
+    RDB.removeDatabase(QSqlDatabase::defaultConnection);
+}
 
-    if(!connOpen())
+bool Login::connOpen()
+{
+    RDB = QSqlDatabase::addDatabase("QSQLITE");
+    //This needs to be your file path. Notice the direction of the "/"
+    RDB.setDatabaseName("C:/Users/Danro/Downloads/RecipeDB/data/RecipeDB.db");
+
+    if(!RDB.open())
     {
-        qDebug()<<"Failed to open DB";
-        return;
+        qDebug()<<("Failed to connect to DB");
+        return false;
     }
-
-    connOpen();
-    QSqlQuery qry;
-    qry.prepare("SELECT * FROM UserInfo WHERE username = '"+Username+"' and password = '"+Password+"'");
-
-    if(qry.exec())
+    else
     {
-        int count = 0;
-        while(qry.next())
-        {
-            count++;
-        }
-        if(count == 1)
-        {
-            ui->label->setText("Username and Password is correct");
-            connClose();
-            this->hide();
-            Recipes recipes;
-            recipes.setModal(true);
-            recipes.exec();
-        }
-        if(count > 1)
-        {
-            ui->label->setText("Duplicate Username and Password");
-        }
-        if(count < 1)
-        {
-            ui->label->setText("Username and Password is not correct");
-        }
-
+        qDebug()<<("Connected to DB");
+        return true;
     }
+}
+
+void Login::on_pushButton_Login_clicked()
+{
+    bool dbOK = this->connOpen();
+    if(dbOK)
+    {
+        QString Username,Password;
+        Username = ui->lineEdit_Username->text();
+        Password = ui->lineEdit_Password->text();
+
+        QSqlQuery qry(RDB);
+        qry.prepare("SELECT * FROM UserInfo WHERE username = '"+Username+"' and password = '"+Password+"'");
+
+        if(qry.exec())
+        {
+            int count = 0;
+            while(qry.next())
+            {
+                count++;
+            }
+            if(count == 1)
+            {
+                ui->label->setText("Username and Password is correct");
+                this->hide();
+                Recipes recipes;
+                QSqlQuery userQry;
+                userQry.exec("SELECT userID FROM UserInfo WHERE username = '" + Username + "'");
+                userQry.next();
+                QSqlRecord record = userQry.record();
+                recipes.setUserId(record.value("userID").toString());
+                connClose();
+                recipes.setModal(true);
+                recipes.exec();
+            }
+            if(count > 1)
+            {
+                ui->label->setText("Duplicate Username and Password");
+                connClose();
+            }
+            if(count < 1)
+            {
+                ui->label->setText("Username and/or Password invalid");
+                connClose();
+            }
+        }
+    }
+    else
+        qDebug() << "Unable to connect to database";
+}
+
+void Login::on_pushButton_Register_clicked()
+{
+    bool dbOK = this->connOpen();
+    if(dbOK)
+    {
+        QString Username,Password;
+        Username = ui->lineEdit_Username->text();
+        Password = ui->lineEdit_Password->text();
+
+        QSqlQuery qry;
+        qry.prepare("SELECT * FROM UserInfo WHERE username = '"+Username+"'");
+
+        if(qry.exec())
+        {
+            int count = 0;
+            while(qry.next())
+            {
+                count++;
+            }
+            if(count >= 1)
+            {
+                ui->label->setText("Duplicate Username");
+                connClose();
+            }
+            if(count == 0)
+            {
+                ui->label->setText("Registered");
+                this->hide();
+                Recipes recipes;
+                QSqlQuery userQry;
+                userQry.exec("INSERT INTO UserInfo(username,password) VALUES ('" + Username + "','" + Password + "')");
+                userQry.exec("SELECT userID FROM UserInfo WHERE username = '" + Username + "'");
+                userQry.next();
+                QSqlRecord record = userQry.record();
+                recipes.setUserId(record.value("userID").toString());
+                connClose();
+                recipes.setModal(true);
+                recipes.exec();
+            }
+        }
+    }
+    else
+        qDebug() << "Unable to connect to database";
 }
