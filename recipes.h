b6d13cd303b30006e93088f78bd92b54358683bd
Chelsea Patek <pateca26@uwgb.edu>
2016-05-13 15:33:41 -0500

    Update recipes.h

--- recipes.h
+++ recipes.h
@@ -16,19 +16,23 @@ public:
 
     explicit Recipes(QWidget *parent = 0);
     ~Recipes();
+    void setUserId(QString id);
+    void updateRecipe();
 
 private slots:
-    void on_pushButton_clicked();
+    void on_pushButton_Save_clicked();
 
-    void on_pushButton_Edit_clicked();
+    void on_tableView_Recipes_clicked(const QModelIndex &index);
 
-    void on_pushButton_load_clicked();
+    void on_pushButton_Clear_clicked();
 
+    void on_pushButton_Delete_clicked();
 
+    void on_pushButton_Search_clicked();
 
 private:
     Ui::Recipes *ui;
-
+    QString userID;
 };
 
 #endif // RECIPES_H
